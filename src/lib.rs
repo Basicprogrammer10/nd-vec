5a74a40d2bc794ed37dec6a6ab6822ecf80b0add
Connor Slade <connor@connorcode.com>
2023-10-27 17:42:13 -0400

    n-dimensional vectors
    Signed-off-by: Connor Slade <connor@connorcode.com>

---
+++ src/lib.rs
@@ -0,0 +1,152 @@
+#![feature(decl_macro)]
+
+use std::{
+    fmt::{Debug, Display},
+    iter::Sum,
+    ops::{Add, Div, Mul, Neg, Rem, Sub},
+};
+
+use num_traits::{real::Real, Num};
+
+#[cfg(test)]
+mod test;
+
+// == 2D Vector ==
+pub type Vector2f32 = Vector<f32, 2>;
+pub type Vector2f64 = Vector<f64, 2>;
+// == 3D Vector ==
+pub type Vector3f32 = Vector<f32, 3>;
+pub type Vector3f64 = Vector<f64, 3>;
+
+#[derive(Clone)]
+pub struct Vector<T, const N: usize> {
+    components: [T; N],
+}
+
+pub macro vector($($x:expr),*) {
+    Vector::new([$($x),*])
+}
+
+impl<T, const N: usize> Vector<T, N> {
+    pub fn new(components: [T; N]) -> Self {
+        Self { components }
+    }
+}
+
+impl<T: Num + Copy, const N: usize> Vector<T, N> {
+    pub fn hadamard_product(&self, other: &Self) -> Self {
+        let mut components = [T::zero(); N];
+        for (i, e) in components.iter_mut().enumerate() {
+            *e = self.components[i] * other.components[i];
+        }
+        Self { components }
+    }
+}
+
+impl<T: Num + Copy + Sum, const N: usize> Vector<T, N> {
+    pub fn magnitude_squared(&self) -> T {
+        self.components.into_iter().map(|x| x * x).sum()
+    }
+
+    pub fn dot(&self, other: &Self) -> T {
+        self.components
+            .iter()
+            .zip(other.components.iter())
+            .map(|(a, b)| *a * *b)
+            .sum()
+    }
+}
+
+impl<T: Num + Copy + Sum + Real, const N: usize> Vector<T, N> {
+    pub fn magnitude(&self) -> T {
+        self.magnitude_squared().sqrt()
+    }
+
+    pub fn normalize(&self) -> Self {
+        *self / self.magnitude()
+    }
+}
+
+impl<T: Num + Copy + Display, const N: usize> Debug for Vector<T, N> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let components = self.components.map(|x| x.to_string()).join(", ");
+        f.write_fmt(format_args!("({})", components))
+    }
+}
+
+impl<T: Num + Copy, const N: usize> Copy for Vector<T, N> {}
+impl<T: Num + Copy, const N: usize> Eq for Vector<T, N> {}
+
+impl<T: Num + Copy, const N: usize> FromIterator<T> for Vector<T, N> {
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        let mut components = [T::zero(); N];
+        for (i, item) in iter.into_iter().enumerate() {
+            components[i] = item;
+        }
+        Self { components }
+    }
+}
+
+macro bin_op($trait:tt, $func:ident) {
+    impl<T: Num + Copy, const N: usize> $trait for Vector<T, N> {
+        type Output = Self;
+
+        fn $func(self, other: Self) -> Self::Output {
+            let mut components = [T::zero(); N];
+            for (i, e) in components.iter_mut().enumerate() {
+                *e = self.components[i].$func(other.components[i]);
+            }
+            Self { components }
+        }
+    }
+
+    impl<T: Num + Copy, const N: usize> $trait<T> for Vector<T, N> {
+        type Output = Self;
+
+        fn $func(self, other: T) -> Self::Output {
+            let mut components = [T::zero(); N];
+            for (i, e) in components.iter_mut().enumerate() {
+                *e = self.components[i].$func(other);
+            }
+            Self { components }
+        }
+    }
+}
+
+bin_op!(Add, add);
+bin_op!(Sub, sub);
+bin_op!(Div, div);
+bin_op!(Rem, rem);
+
+impl<T: Num + Copy, const N: usize> Neg for Vector<T, N> {
+    type Output = Self;
+
+    fn neg(self) -> Self::Output {
+        let mut components = [T::zero(); N];
+        for (i, e) in components.iter_mut().enumerate() {
+            *e = T::zero() - self.components[i];
+        }
+        Self { components }
+    }
+}
+
+impl<T: Num + Copy, const N: usize> PartialEq for Vector<T, N> {
+    fn eq(&self, other: &Self) -> bool {
+        self.components
+            .iter()
+            .zip(other.components.iter())
+            .all(|(a, b)| a == b)
+    }
+}
+
+impl<T: Num + Copy + Send + Sync, const N: usize> Mul<T> for Vector<T, N> {
+    type Output = Self;
+
+    fn mul(self, rhs: T) -> Self::Output {
+        let mut components = [T::zero(); N];
+        for (i, e) in components.iter_mut().enumerate() {
+            *e = self.components[i] * rhs;
+        }
+        Self { components }
+    }
+}
---
+++ src/test.rs
@@ -0,0 +1,47 @@
+use super::*;
+
+#[test]
+fn test_bin_ops() {
+    let a = vector!(1, 2, 3);
+    let b = vector!(4, 5, 6);
+
+    assert_eq!(a + b, vector!(5, 7, 9));
+    assert_eq!(a - b, vector!(-3, -3, -3));
+    assert_eq!(a / b, vector!(0, 0, 0));
+    assert_eq!(a % b, vector!(1, 2, 3));
+}
+
+#[test]
+fn test_unary_ops() {
+    let a = vector!(1, 2, 3);
+
+    assert_eq!(-a, vector!(-1, -2, -3));
+}
+
+#[test]
+fn test_scalar_ops() {
+    let a = vector!(1, 2, 3);
+
+    assert_eq!(a + 2, vector!(3, 4, 5));
+    assert_eq!(a - 2, vector!(-1, 0, 1));
+    assert_eq!(a / 2, vector!(0, 1, 1));
+    assert_eq!(a % 2, vector!(1, 0, 1));
+    assert_eq!(a * 2, vector!(2, 4, 6));
+}
+
+#[test]
+fn test_norms() {
+    let a = vector!(1.0, 2.0, 3.0);
+
+    assert_eq!(a.magnitude_squared(), 14.0);
+    assert_eq!(a.magnitude(), 14f64.sqrt());
+}
+
+#[test]
+fn test_product() {
+    let a = vector!(1, 2, 3);
+    let b = vector!(4, 5, 6);
+
+    assert_eq!(a.dot(&b), 32);
+    assert_eq!(a.hadamard_product(&b), vector!(4, 10, 18));
+}
